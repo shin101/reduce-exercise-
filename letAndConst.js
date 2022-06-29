011b2618354c12276fd41b9d0abd6c41119ae4f1
Shin <jeesoshn@88665a4a6164.ant.amazon.com>
2022-06-28 23:08:50 -0700

    first commit for let and const homework

---
+++ letAndConst.js
@@ -0,0 +1,10 @@
+// Question 1 
+
+// const PI = 3.14;
+// PI = 42; 
+
+// Quiz
+// 1. With var you can redeclare and assign variables 
+// 2. Const is constant, once declared cannot redeclare whereas for var you can
+// 3. Const is constant, cannot reassign value 
+// 4. Hoisted is when you can access and use a variable before it is defined. This is the case for var
